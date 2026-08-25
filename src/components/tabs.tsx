74ca2403320b925b068285e98354d8c7273f79cb
jaminliu89 <jaminliu89@outlook.com>
2026-08-25 23:01:37 +0800

    feat(runtime): add Radix Tabs component

---
+++ src/components/tabs.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import { cn } from "../lib/cn";
+
+export const Tabs = TabsPrimitive.Root;
+
+export const TabsList = React.forwardRef<React.ElementRef<typeof TabsPrimitive.List>, React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>>(function TabsList({ className, ...props }, ref) {
+  return <TabsPrimitive.List ref={ref} className={cn("p-tabs", className)} {...props} />;
+});
+
+export const TabsTrigger = React.forwardRef<React.ElementRef<typeof TabsPrimitive.Trigger>, React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>>(function TabsTrigger({ className, ...props }, ref) {
+  return <TabsPrimitive.Trigger ref={ref} className={cn("p-tab", className)} {...props} />;
+});
+
+export const TabsContent = React.forwardRef<React.ElementRef<typeof TabsPrimitive.Content>, React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>>(function TabsContent({ className, ...props }, ref) {
+  return <TabsPrimitive.Content ref={ref} className={cn("p-tab-content", className)} {...props} />;
+});
